bdcbd58f5d64a30e093c9b870b12efaf77286558
Aileen Donegan <aileendonegan@Aileens-MacBook-Air.local>
2020-01-17 17:14:43 +0000

    fixed typo

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog28.herokupapp.com']
+ALLOWED_HOSTS = ['django-blog28.herokuapp.com']
 
 
 # Application definition
