b9a6161efe8d22e206d1009dfa1b03a5915e8652
Aileen Donegan <aileendonegan@Aileens-MacBook-Air.local>
2020-01-17 13:28:16 +0000

    Fixed typos in html / view

--- posts/views.py
+++ posts/views.py
@@ -13,7 +13,7 @@ def get_posts(request):
 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
               "-published_date")
-    return render(request, "blogpost.html", {'posts': posts})
+    return render(request, "blogposts.html", {'posts': posts})
 
 
 def post_detail(request, pk):
